3e39253f7ad08bc064ec607e4a299bc340e9633d
openfirevip <87262643+openfirevip@users.noreply.github.com>
2021-07-14 16:31:15 +0800

    Update ofe.sol
    add event

--- hrc20/ofe.sol
+++ hrc20/ofe.sol
@@ -5,6 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract ofe is ERC20 {
     
     address public owner;
+	
+	event SEO(uint256 totalSupply);
     
     constructor(string memory _name, string memory _symbol, uint256 _totalSupply) ERC20(_name, _symbol) {
         owner = msg.sender;
@@ -13,6 +15,7 @@ contract ofe is ERC20 {
     
     function seo(uint256 _totalSupply) public onlyOwner returns(bool) {
         _mint(msg.sender, _totalSupply*10**18);
+		emit SEO(_totalSupply*10**18); 
         return true;
     }
     
